f331e0a2a4b9cdd093d50930a66bc4db7be897ad
高木祐介 <nya.takasan@gmail.com>
2013-07-20 15:31:02 +0900

    とりあえず、openspeding.comのデータを取り込む設定にした。

--- js/setting.js
+++ js/setting.js
@@ -10,8 +10,8 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'yokohama_budget';
-OpenSpending.year = '2012';
+OpenSpending.identifier = 'sna_national_accounts_of_japan';
+OpenSpending.year = '2011';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
 
